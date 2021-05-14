fd9f499fb907a0dd7f4583f0a56420e205f9f10e
Chaitanya Kumar <chaitanyak28771@gmail.com>
2021-05-14 14:14:07 +0530

    Obstacle initialization tests failing. Fix

--- src/obstacle.hpp
+++ src/obstacle.hpp
@@ -29,7 +29,6 @@ constexpr double u2_y {-0.8660254037844386};
 // constexpr double u2_y {-sin(PI/3.0)};
 
 
-__host__ __device__
 double
 lattice_vector_length(const size_t d_1, const size_t d_2, const double b1_x, const double b1_y,
                       const double b2_x, const double b2_y)
@@ -39,20 +38,32 @@ lattice_vector_length(const size_t d_1, const size_t d_2, const double b1_x, con
     return (sqrt(rv_x*rv_x + rv_y*rv_y));
 }
 
+
+__device__
+double
+lattice_vector_length_d(const size_t d_1, const size_t d_2, const double b1_x, const double b1_y,
+                      const double b2_x, const double b2_y)
+{
+    const double rv_x { d_1*b1_x + d_2*b2_x };
+    const double rv_y { d_1*b1_y + d_2*b2_y };
+    return (sqrt(rv_x*rv_x + rv_y*rv_y));
+}
+
+
 template <typename word>
 __global__
 void
 init_cyl_obst_kernel(word *buffer, size_t width, size_t height,
         size_t centre_x, size_t centre_y, double radius)
 {
-    const auto row { blockDim.y * blockIdx.y + threadIdx.y };
-    const auto col { blockDim.x * blockIdx.x + threadIdx.x };
+    const auto row = blockDim.y*blockIdx.y + threadIdx.y;
+    const auto col = blockDim.x * blockIdx.x + threadIdx.x;
 
     if (row < height && col < width)
     {
-        const auto d_1 { labs(col - centre_x) };
-        const auto d_2 { labs(row - centre_y) };
-        const auto c_dist = lattice_vector_length(d_1, d_2, u1_x, u1_y, u2_x, u2_y);
+        const auto d_1 = labs(col - centre_x);
+        const auto d_2 = labs(row - centre_y);
+        const auto c_dist = lattice_vector_length_d(d_1, d_2, u1_x, u1_y, u2_x, u2_y);
         buffer[row*width + col] = (c_dist <= radius ? 1 : 0);
     }
 
@@ -70,13 +81,18 @@ initialize_cylindrical_obstacle(word *buffer, const size_t width, const size_t h
     const size_t mem_sz = width * height * sizeof(word);
    
     word *device_buffer;
-    cudaMemcpy((void **) &device_buffer, buffer, mem_sz, cudaMemcpyHostToDevice);
+    cudaMalloc((void **) device_buffer, mem_sz);
+    gpuErrchk( cudaGetLastError() );
+    cudaMemcpy(device_buffer, buffer, mem_sz, cudaMemcpyHostToDevice);
+    gpuErrchk( cudaGetLastError() );
 
     init_cyl_obst_kernel<word><<<grid_config, block_config>>>(device_buffer, width, height,
             centre_x, centre_y, radius);
 
     cudaMemcpy(buffer, device_buffer, mem_sz, cudaMemcpyDeviceToHost);
+    gpuErrchk( cudaGetLastError() );
     cudaFree(device_buffer);
+    gpuErrchk( cudaGetLastError() );
     return 0;
 error:
     return 1;
--- tests/obstacle_tests.cu
+++ tests/obstacle_tests.cu
@@ -1,21 +1,38 @@
+#include "dbg.h"
 #include "minunit.h"
 #include "obstacle.hpp"
+#include "helper_types.hpp"
 #include "helper_functions.hpp"
 
-std::string filename {"tests/obstacle_tests.dat"};
+
+constexpr size_t width  {100};
+constexpr size_t height {100};
+constexpr double radius { (double) (min<size_t>(width, height)/4) };
+constexpr size_t c_x { width / 2 };
+constexpr size_t c_y { height/ 2 };
 
 const char *test_lattice_vector_length()
 {
     const auto len1 {lattice_vector_length(1,0, u1_x, u1_y, u2_x, u2_y)};
     mu_assert(len1 == 1.0, "Incorrect lattice vector length calculation");
     const auto len2 = lattice_vector_length(1,1,u1_x, u1_y, u2_x, u2_y);
-    debug("1,1: %lf", len2);
     mu_assert(fabs(len2 - 1.0) < 0.0001, "Incorrect lattice vector length calculation");
+    const auto len3 { lattice_vector_length(3, 6, u1_x, u1_y, u2_x, u2_y) }; 
+    mu_assert(flerror(len3, 5.19615242) < 0.001, "Incorrect lattice vector length calculation");
     return NULL;
 }
 
 const char *test_obstacle_creation()
 {
+    log_info("Not really a test. Check printed output");
+    int *buff = new int[width * height];
+    
+    initialize_cylindrical_obstacle<int>(buff, width, height, c_x, c_y, radius);
+    cudaDeviceSynchronize();
+    gpuErrchk(cudaGetLastError( ));
+
+    print_grid(buff, width, height);
+    delete[] buff;
     return NULL;
 }
 
@@ -24,6 +41,7 @@ const char *all_tests()
 {
     mu_suite_start();
     mu_run_test(test_lattice_vector_length);
+    // mu_run_test(test_obstacle_creation);
     return NULL;
 }
 
