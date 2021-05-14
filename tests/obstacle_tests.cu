15a2b1c3b79e69480db9eac0f4248580f4131e1f
Chaitanya Kumar <chaitanyak28771@gmail.com>
2021-05-14 12:47:03 +0530

    Start obstacle creation tests

---
+++ tests/obstacle_tests.cu
@@ -0,0 +1,19 @@
+#include "minunit.h"
+#include "obstacle.hpp"
+#include "helper_functions.hpp"
+
+
+char *test_obstacle_creation()
+{
+    
+}
+
+
+char *all_tests()
+{
+    mu_suite_start();
+
+    return NULL;
+}
+
+RUN_TESTS(all_tests);
